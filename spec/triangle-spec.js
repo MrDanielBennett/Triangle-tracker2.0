d5c840ee3fb60d28225d0f48e556da2e596fe053
Ben Kirby and Daniel Bennett <ben@>
2019-01-08 10:35:04 -0800

    Now has passing specs for all triangle types

--- spec/triangle-spec.js
+++ spec/triangle-spec.js
@@ -1,8 +1,23 @@
 import { Triangle } from './../src/triangle.js';
 describe('Triangle', function() {
 
-  it('should test if a triangle is possible', function(){
+  it('It should test if a triangle is impossible', function(){
     let newTriangle = new Triangle(0, 1, 3);
     expect(newTriangle.isTriangle()).toEqual(false);
+  });
+
+  it('It should test what type of triangle', function(){
+    let newTriangle = new Triangle(3, 3, 3);
+    expect(newTriangle.triangleType()).toEqual("equilateral");
+  });
+
+  it('It should test what type of triangle', function(){
+    let newTriangle = new Triangle(2, 3, 3);
+    expect(newTriangle.triangleType()).toEqual("isosceles");
+  });
+
+  it('It should test what type of triangle', function(){
+    let newTriangle = new Triangle(2, 3, 4);
+    expect(newTriangle.triangleType()).toEqual("scalene");
   });
 });
